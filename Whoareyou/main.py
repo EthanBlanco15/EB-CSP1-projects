d2bd45371dedd5da2b323f1ea2756e0d0f8716ca
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-09-11 10:19:32 -0600

    Adding this onto my Whoareyou assignment

---
+++ Whoareyou/main.py
@@ -0,0 +1,6 @@
+#Ethan Blanco; Skill practice: Who are you?
+
+name = input("What is your name?;")
+age = input("What is your age?;")
+color = input("What is your favorite color?;")
+print("Hello", name, "you are", age, "years old,", "Eand your favorite color is", color)
