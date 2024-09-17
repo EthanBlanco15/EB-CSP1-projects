94ec223845888b1ce1dedc68bdf6cc8d8dce9f81
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-09-17 10:25:09 -0600

    Setting, Resetting and Resetting. Ethan Blanco

---
+++ Setting, Resetting, and Resetting/main.py
@@ -0,0 +1,13 @@
+#Ethan Blanco, SkillPractice: Setting, Resetting and Resetting
+
+staff = 32
+students=100
+students = students -1
+guests=students*2
+seats = 12
+
+staff -=3
+guests=students*2 - 15
+staff +=1
+
+print((staff + students+guests)/seats)
