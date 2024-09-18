ae1e58893fb78478d09a07ad11595a98169d7b32
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-09-18 10:19:53 -0600

    ProficiencyTest: Celsius to Fahrenheit, Ethan Blanco

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,6 @@
+#Ethan Blanco, ProficiencyTest: Celsius to Fahrenheit
+celsius = input("Enter temperature in Celsius;")
+celsius = int(celsius)
+fahrenheit = (celsius*9/5)+32
+str(fahrenheit)
+print("The temperature in celsius", celsius, "to fahrenheit is", fahrenheit)
