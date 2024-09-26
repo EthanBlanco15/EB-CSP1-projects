a404c78c3d84f14f4fbac74bbde46e435d68d8e8
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-09-26 10:13:40 -0600

    Ethan Blanco, ProficiencyTest: Simple Calculator

---
+++ simple calculator/main.py
@@ -0,0 +1,23 @@
+#Ethan Blanco, ProfiencyTest: Simple Calculator
+
+num1 = input("Enter your first arithmetic number;")
+num1 = int(num1)
+num2 = input("Enter your second arithmetic number;")
+num2 = int(num2)
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 // num2)
+print(num1 % num2)
+
+
+
+#If/Else practice
+#if (variable "add") :
+#   print(num1 + num2)
+
+#elif (variable "subtract") :
+#   print(num1 - num2)
