ac07e919ec98a75b8d6209eb88689337c3ba9420
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-09-27 15:54:27 -0600

    completed madlib

---
+++ Madlib/main.py
@@ -0,0 +1,14 @@
+# Blanco, Proficiency Test: Madlib
+
+name = input("What is your name? ")
+money = input("How much money do you have?(say dollars or cents with the amount) ")
+weapon = input("What is your favorite weapon?(asking for no particular reason at all) ")
+food = input("What is a food you would be eating right now? ")
+friends = input("How many friends do you bring with you on your journey? ")
+
+print( name, "walks down a street in Gotham city, where he encounters a gang of thugs, and they demand money."
+       "The leader says, Give me all of your money! I know you got", money, ", so hand it over!", 
+        name, "wonders how they know how much he has down to the cent, and decide to take out your favorite",
+        weapon, "! as you do it, they start to become scared, before they even see you take it out." 
+        "The thugs say, Wait! We know you have a", weapon, "! We can see those stuff up there! Your name, your money, your weapon, that,", food, "and that you got", friends, "with you right now!"
+        "Please we/’ll return your money!-RECEIVED", money,)
---
+++ graded quiz/main.py
@@ -0,0 +1,17 @@
+#Ethan Blanco, ProficiencyTest: Graded Quiz.
+
+mars = input("Do we live on Mars? ")
+heart = input("Do we have a heart? ")
+english = input("Do we speak English? ")
+question = input("Is this a question? ")
+seven = input("Is seven a lucky number? ")
+
+score = 0
+
+if mars == "no" : + 1
+if heart == "yes" : + 1
+if english == "yes" : + 1
+if question == "yes" : + 1
+if seven == "yes" : + 1
+
+print(score)
