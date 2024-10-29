c045c3b7c434329ba2aa36f690e22b85d5565680
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-10-29 10:51:47 -0600

    Ethan Blanco, ProficiencyTest: Shopping List Manager

--- shopping list manager/main.py
+++ shopping list manager/main.py
@@ -1,16 +1,34 @@
 #Ethan Blanco, ProficiencyTest: Shopping List Manager
+list = [] #Makes the variable list for what they need to add.
+def add(): #Defining the add() below
+    object = input("What item do you want to add?") #Works as a variable, object and is asking the user a question.
+    list.append(object) #Adds a variable or in this case an item to the list.
+    print(list) #Obviously, prints out the list to show progress.
 
-cart = input("Would you like to 'add' an item to your shopping cart, 'remove' or quit the program?")
-add = print("What would you like to add to your cart?")
+def remove(): #Defining the remove() below for use.
+    object = input("What item do you want to remove?") #Works as a variable like before.
+    list.remove(object) #Rather than adding, you remove a variable from the list, an object.
+    print(list)
 while True:
+
     action = input("""What would you like to do?
-                   Enter 'add' to add item
-                   Enter 'remove' to remove item
-                   Etner 'quit' to leave the list:\n""")
-if action =="add":
-    add()
-elif action =="remove":
-    remove()
-else:
-    print("Have a great day!")
-    #break
+
+                                  Enter 1 to add item
+
+                                  Enter 2 to remove item
+
+                                  Enter 3 to leave the list:\n""")
+
+    if action =="1":
+
+        add()
+
+    elif action =="2":
+
+        remove()
+
+    else:
+
+        print("Have a nice day!")
+
+        break
