b6a2dc4c552991a56915b66856ab55257f4c6b85
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-11-11 10:42:03 -0700

    Ethan Blanco, ProficiencyTest: Secret Cypher

--- secret cypher/main.py
+++ secret cypher/main.py
@@ -1,84 +1,13 @@
 #Ethan Blanco, ProficiencyTest: Secret Cypher
-a = 0
-b = 0
-c = 0
-d = 0
-e = 0
-f = 0
-g = 0
-h = 0
-i = 0
-j = 0
-k = 0
-l = 0
-m = 0
-n = 0
-o = 0
-p = 0
-q = 0
-r = 0
-s = 0
-t = 0
-u = 0
-v = 0
-w = 0
-x = 0
-y = 0
-z = 0
-code = input("What letter would you like to have to shift cypher? In lowercase and one letter only:\n ")
-if code == "a":
-    a =+ 19302
-elif code == "b":
-    b =+ 3153
-elif code == "c":
-    c =+ 5503185
-elif code == "d":
-    d =+ 10835
-elif code == "e":
-    e =+ 643109
-elif code == "f":
-    f =+ 6129
-elif code == "g":
-    g =+ 39201
-elif code == "h":
-    h =+ 73092
-elif code == "i":
-    i =+ 8202958
-elif code == "j":
-    j =+ 901424
-elif code == "k":
-    k =+ 2194023
-elif code == "l":
-    l =+ 353159
-elif code == "m":
-    m =+ 614370
-elif code == "n":
-    n =+ 315804
-elif code == "o":
-    o =+ 736491
-elif code == "p":
-    p =+ 903149
-elif code == "q":
-    q =+ 315853931903
-elif code == "r":
-    r =+ 90315834
-elif code == "s":
-    s =+ 7139482
-elif code == "t":
-    t =+ 809124805
-elif code == "u":
-    u =+ 51531580
-elif code == "v":
-    v =+ 91243853
-elif code == "w":
-    w =+ 4831580
-elif code == "x":
-    x =+ 1380315
-elif code == "y":
-    y =+ 71438530
-elif code == "z":
-    z =+ 413258035
-else:
-    print("Invalid prompt")
-code = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-print("Here's your code!\n", code)
+
+code =input("What would you like your code to be? Please input an available word.\n ")
+cypher = "" # Empty string
+def converter(code, cypher):
+    for letter in code: # Normally, ord and chr functions only take one identation, but with the for loop, it takes it letter by letter.
+        letter = ord(letter) # Ord turns strings into integers, thats the conversion here.
+        letter += 1 # We're adding one extra here to make it more mixed.
+        letter = chr(letter) # using the chr function here converts the integers back into a string.
+        cypher += letter #Adding the empty string and function of chr and ord.
+    print(cypher) # We left the print function here, but made the identation less to not include everything.
+
+converter(code, cypher) 
