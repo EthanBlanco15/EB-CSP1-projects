a5d1dc0819ac199f1ed112e1be515a314c7f68f9
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-11-15 10:49:40 -0700

    Ethan Blanco, ProficiencyTest: Multiplication Table

--- multiplication table/main.py
+++ multiplication table/main.py
@@ -1,2 +1,5 @@
 #Ethan Blanco, ProficiencyTest: Multiplication Table
 
+num = int(input("From 0-12, which number would you like to see the multiples of?\n "))
+for factor in range(13): #The factor here times everything from 1-12
+    print(num * factor) #This is multiplying the input by numbers the factor represents which is 1-12
