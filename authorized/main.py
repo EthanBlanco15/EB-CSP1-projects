3db598f370e3fe318b00339d12198059c7fe4d09
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-11-18 10:15:36 -0700

    Ethan Blanco, ProficiencyTest: Authorized

--- authorized/main.py
+++ authorized/main.py
@@ -1,5 +1,24 @@
 #Ethan Blanco, ProficiencyTest: Authorized
 
 users = ["abc404", "zenith", "Bob", "nagi", "Leo", "gen", "Shin", "appleplayer"]
-print(users)
-auth = input("")
+auth = input("Enter your username:\n ")
+if auth == "":
+    print("No input seen.")
+elif auth == "abc404":
+    print("Hello abc404, welcome back.\n ")
+elif auth == "zenith":
+    print("Hello zenith, welcome back.\n ")
+elif auth == "Bob":
+    print("Sorry Bob, you do not have an authorized account, would you like to register?\n ")
+elif auth == "nagi":
+    print("Welcome back Admin user nagi.\n ")
+elif auth == "Leo":
+    print("Hello Leo, welcome back.\n ")
+elif auth == "gen":
+    print("Hello gen, welcome back.\n ")
+elif auth == "Shin":
+    print("Welcome back Admin user Shin.\n ")
+elif auth == "appleplayer":
+    print("Sorry apple player, you do not have an authorized account, would you like to register?\n ")
+else: 
+    print("This isn't available right now, use another input.")
--- count up and down/main.py
+++ count up and down/main.py
@@ -2,7 +2,7 @@
 
 num = input("Please input 20 to see it count up and down! Remember to put 21 to see it go untill 20:\n ")
 for num in range(20):
-    print(num)
+    print(num) #Or for num in range(5, 0, -1)
 else:
     for num1 in range(21):
         print(-num1 + 20)
