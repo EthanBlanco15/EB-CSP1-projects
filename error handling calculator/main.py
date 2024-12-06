046e78ec1d3a9b38b0ccb5595e4686987a6a85cb
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2024-12-06 10:51:44 -0700

    Ethan Blanco, SkillPractice: Error Handling Calculator

--- error handling calculator/main.py
+++ error handling calculator/main.py
@@ -14,13 +14,29 @@ while True:
         continue
 
     try:
-        print(num1 + num2)
-        print(num1 - num2)
-        print(num1 * num2)
-        print(num1 / num2)
-        print(num1 ** num2)
-        print(num1 // num2)
-        print(num1 % num2)
+        calculator = input("""What arithmetic operator would you like to use?
+            a. Addition               
+            b. Subtraction               
+            c. Multiplication               
+            d. Division
+            e. Exponention
+            f. Floor division
+            g. Module Operator                              
+                           \n""")
+        if calculator =="a":
+            print(num1 + num2)
+        if calculator =="b":
+            print(num1 - num2)
+        if calculator =="c":
+            print(num1 * num2)
+        if calculator =="d":
+            print(num1 / num2)
+        if calculator =="e":
+            print(num1 ** num2)
+        if calculator =="f":
+            print(num1 // num2)
+        if calculator =="g":
+            print(num1 % num2)
     except:
         if num2 ==0:
             print("This doesn't work properly.")
